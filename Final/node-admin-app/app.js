234aaa431ab6b723d785922ef197b3cf367aecf8
Rhoyunseok <74577789+Rhoyunseok@users.noreply.github.com>
2024-07-30 11:12:55 +0900

    fianl update

--- Final/node-admin-app/app.js
+++ Final/node-admin-app/app.js
@@ -8,8 +8,8 @@ var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var adminRouter = require('./routes/admin');
+var articleRouter = require('./routes/article');
 
 var app = express();
 
@@ -32,8 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use('/admin', adminRouter);
+app.use('/article', articleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Final/node-admin-app/routes/admin.js
+++ Final/node-admin-app/routes/admin.js
@@ -1,172 +1,113 @@
+//admin.js 라우터의 기본주소는 
+//http://localhost:5001/admin 주소로 app.js에서 설정되어 있다.
 var express = require('express');
 var router = express.Router();
 
-
-/* 
-- 관리자계정목록 웹페이지 요청과 응답처리 라우팅메소드
+/*
+- 관리자 계정 목록 조회 웹페이지 요청과 응답처리 라우팅메소드
 - 요청주소: http://localhost:5001/admin/list
 - 요청방식: Get
-- 응답결과: admin/list.ejs 뷰페이지 반환
+- 응답결과: 관리자계정 목록 조회 웹페이지(뷰+Data) 반환
 */
-router.get('/list', async(req, res, next)=> {
+router.get('/list', async(req, res, next)=>{
+    res.render('admin/list.ejs');
+});
 
+/*
+- 관리자 계정 목록 조회처리 웹페이지 요청과 응답처리 라우팅메소드
+- 요청주소: http://localhost:5001/admin/list
+- 요청방식: Post
+- 응답결과: 관리자 계정 조회옵션 결과 웹페이지(뷰+Data) 반환
+*/
+router.post('/list',async(req,res)=>{
+    //Step1:조회 옵션정보 추출하기
 
+    //Step2:조회옵션으로 관리자정보 조회하기
 
-
-    const admins = [
-        {
-            admin_no:1,
-            admin_id:"admin1",
-            admin_name:"관리자1",
-            admin_email:"admin1@kkk",
-            admin_company:"CBNU",
-            admin_cp:"소프트웨어학과",
-            admin_use:1,
-            admin_regist_date:Date.now(),
-            admin_description:"관리자1입니다."
-        },
-        {
-            admin_no:2,
-            admin_id:"admin2",
-            admin_name:"관리자2",
-            admin_email:"admin2@kkk",
-            admin_company:"충북대",
-            admin_cp:"전자정보대",
-            admin_use:2,
-            admin_regist_date:Date.now(),
-            admin_description:"관리자2입니다."
-        },
-        {
-            admin_no:3,
-            admin_id:"admin3",
-            admin_name:"관리자3",
-            admin_email:"admin3@kkk",
-            admin_company:"충북대학교",
-            admin_cp:"미지정",
-            admin_use:2,
-            admin_regist_date:Date.now(),
-            admin_description:"관리자3입니다."
-        },
-
-    ];
-
-
-
-    // res.json(apiResult);
-    res.render('admin/list.ejs', {admins:admins});
+    //Step3:조회결과데이터 뷰에전달하기
+    res.render('admin/list.ejs');
 });
 
 
-
-
-
-/* 
-- 관리자계정 신규등록 웹페이지 요청과 응답처리 라우팅메소드
+/*
+- 신규 관리자 계정 등록 웹페이지 요청과 응답처리 라우팅메소드
 - 요청주소: http://localhost:5001/admin/create
 - 요청방식: Get
-- 응답결과: admin/create.ejs 뷰페이지 반환
+- 응답결과: 신규 관리자 계정 등록 웹페이지(뷰) 반환
 */
 router.get('/create', async(req, res, next)=> {
-    res.render('admin/create.ejs');
+    res.render('admin/create');
 });
 
-/* 
-- 관리자계정 신규등록 웹페이지 요청과 응답처리 라우팅메소드
+
+/*
+- 신규 관리자 정보 등록처리 요청과 응답처리 라우팅메소드
 - 요청주소: http://localhost:5001/admin/create
-- 요청방식: post
-- 응답결과: 목록페이지 이동
+- 요청방식: Post
+- 응답결과: 신규 관리자 계정 등록 후 목록 페이지 이동
 */
 router.post('/create', async(req, res, next)=> {
-    let apiResult = {
-        code:200,
-        data:null,
-        result:""
-    };
-    try{
-        const admin_id = req.body.admin_id;
-        const admin_name = req.body.admin_name;
-        const admin_email = req.body.admin_email;
-        const admin_company = req.body.admin_company;
-        const admin_cp = req.body.admin_cp;
-        const admin_use = req.body.admin_use;
-        // const admin_regist_date = req.body.admin_regist_date;
-        const admin_description = req.body.admin_description;
 
-        const admin = {
-            admin_id:admin_id,
-            admin_name:admin_name,
-            admin_email:admin_email,
-            admin_company:admin_company,
-            admin_cp:admin_cp,
-            admin_use:admin_use,
-            // admin_regist_date:admin_regist_date,
-            admin_description:admin_description
-        };
+    //Step1: 신규 관리자 정보 추출하기
+    // const id = req.body.id;
+    // const pw = req.body.pw;
+    // const code = req.body.code;
 
-        const dbAdmin = {
-            admin_no:1,
-            admin_id:admin_id,
-            admin_name:admin_name,
-            admin_email:admin_email,
-            admin_company:admin_company,
-            admin_cp:admin_cp,
-            admin_use:admin_use,
-            // admin_regist_date:admin_regist_date,
-            admin_description:admin_description
-        };
-        apiResult.code = 200;
-        apiResult.data = dbAdmin;
-        apiResult.result = "ok";
+    //Step2: 신규 관리자 정보 DB저장 처리
 
-    }catch{
-        apiResult.code = 500;
-        apiResult.data = null;
-        apiResult.result = "Server Error or 관리자에게 문의하세요.ㄹㅇㅋㅋ";
-    }
+    //Step3: 목록 페이지로 이동시키기
     res.redirect('/admin/list');
-
 });
 
+
+/*
+- 기존 관리자 정보 수정처리 요청과 응답처리 라우팅메소드
+- 요청주소: http://localhost:5001/admin/modify
+- 요청방식: Post
+- 응답결과: 기존 관리자 계정 정보 수정처리 후 목록 페이지 이동
+*/
+router.post('/modify', async(req, res, next)=> {
+
+    //STEP1: 사용자 수정데이터 추출하기
+    // const id = req.body.id;
+    // const pw = req.body.pw;
+    // const code = req.body.code;
+
+    //STEP2: DB 해당 관리자 계정 수정처리하기 
+
+
+    //STEP3: 수정처리후 목록페이지로 이동처리
+    res.redirect('/admin/list');
+});
+
+
+
+/*
+- 기존 관리자 정보 삭제처리 요청과 응답처리 라우팅메소드
+- 요청주소: http://localhost:5001/admin/delete?id=1
+- 요청방식: Get
+- 응답결과: 기존 관리자 계정 정보 삭제처리 후 목록 페이지 이동
+*/
 router.get('/delete', async(req, res, next)=> {
     res.redirect('/admin/list');
 });
 
 
-router.post('/modify/', async(req, res, next)=> {
-    const admin_no = req.body.admin_no;
+/*
+- 기존 관리자 정보 확인 웹페이지 요청과 응답처리 라우팅메소드
+- 요청주소: http://localhost:5001/admin/modify/1
+- 요청방식: Get
+- 응답결과: 기존 관리자 계정 정보가 포함된 웹 페이지(뷰) 제공
+*/
+router.get('/modify/:id', async(req, res, next)=> {
 
-    const admin = {
-        
-        admin_id:req.body.admin_id,
-        admin_name:req.body.admin_name,
-        admin_email:req.body.admin_email,
-        admin_company:req.body.admin_company,
-        admin_cp:req.body.admin_cp,
-        admin_use:req.body.admin_use,
-        admin_regist_date:Date.now(),
-        admin_description:req.body.admin_description
-    };
-    res.redirect('/admin/list');
-    
+    //step1: URL에서 관리자 고유번호를 추출합니다.
+
+    //step2: 단일 관리자 정보를 db에서 조회해옵니다. 
+
+    //step3: 단일 관리자 정보를 뷰에 전달합니다.
+    res.render('admin/modify');
 });
 
-router.get('/modify/:no', async(req, res, next)=> {
-
-    const admin_no = req.params.admin_no;
-
-    const admin = {
-        admin_no:1,
-        admin_id:"admin1",
-        admin_name:"관리자1",
-        admin_email:"admin1@kkk",
-        admin_company:"CBNU",
-        admin_cp:"소프트웨어학과",
-        admin_use:1,
-        admin_regist_date:Date.now(),
-        admin_description:"관리자1입니다."
-    };
-
-    res.render('admin/modify.ejs',{admin:admin});
-});
 
 module.exports = router;
